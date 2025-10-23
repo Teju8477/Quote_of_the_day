ff66fc14bbe89e857136bba7a4125ddab999f727
Tejaswini Vanam <teju0308.vanam@gmail.com>
2025-10-23 11:31:43 +0530

    Initial commit: qoute of the day app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template_string
+import random
+
+app = Flask(__name__)
+
+# List of quotes
+quotes = [
+    "Believe in yourself!",
+    "Every day is a second chance.",
+    "Dream big, work hard, stay humble.",
+    "You are capable of amazing things.",
+    "Success starts with self-belief."
+]
+
+@app.route('/')
+def home():
+    quote = random.choice(quotes)
+    return render_template_string('''
+        <html>
+        <head><title>Quote of the Day</title></head>
+        <body style="text-align:center; margin-top:100px; font-family:sans-serif;">
+            <h1>🌟 Quote of the Day 🌟</h1>
+            <h2>{{ quote }}</h2>
+        </body>
+        </html>
+    ''', quote=quote)
+
+if __name__ == '__main__':
+    app.run(debug=True)
